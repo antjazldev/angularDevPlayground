c37e23f3783b67951f3607d0d02fd297e2050b3f
¨Luis¨ <antjazl@gmail.com>
2020-06-01 16:51:34 -0500

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBOzLm8is35jyAkxpIk0nFzXEQ2RNPPmvQ",
+    authDomain: "hoteles-a6cf4.firebaseapp.com",
+    databaseURL: "https://hoteles-a6cf4.firebaseio.com",
+    projectId: "hoteles-a6cf4",
+    storageBucket: "hoteles-a6cf4.appspot.com",
+    messagingSenderId: "658862862830",
+    appId: "1:658862862830:web:0a7673df923a0a403c9eff",
+    measurementId: "G-4H44Q0DES7"
+  },
 };
 
 /*
